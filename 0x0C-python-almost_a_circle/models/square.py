1fd2aede236a96a7b46f9195205f27e606ab0deb
majok31 <majoktot70@gmail.com>
2024-04-24 08:06:15 -0700

    update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -53,8 +53,8 @@ class Square(Rectangle):
                     self.x = value
                 elif key == "y":
                     self.y = value
-                # removed the break statement, incase if the passed args are greater
-                # than 5, and one of the attributes is at the end
+        # removed the break statement, incase if the passed args are greater
+        # than 5, and one of the attributes is at the end
 
     def to_dictionary(self):
         """Return the dictionary representation of the Square."""
