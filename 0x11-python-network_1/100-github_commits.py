0d5874a71dc605d52560e786c44b76df5bc4deef
majok31 <majoktot70@gmail.com>
2024-07-05 11:12:07 -0700

    more tasks

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
 """
-Takes your GitHub credentials (username and password) and uses the GitHub API
-to display your id
+Please list 10 commits (from the most recent to oldest) of the repository
+“rails” by the user “rails”
+You must use the GitHub API, here is the documentation
+https://developer.github.com/v3/repos/commits/
+Print all commits by: `<sha>: <author name>` (one by line)
 """
+
 
 if __name__ == '__main__':
     from requests import get
     from sys import argv
 
-    username = argv[1]
-    password = argv[2]
+    repo = argv[1]
+    owner = argv[2]
+    i = 0
 
-    URL = "https://api.github.com/user"
-    response = get(URL, auth=(username, password))
+    URL = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
+
+    response = get(URL)
     json = response.json()
 
-    print(json.get('id'))
+    for element in json:
+        if i > 9:
+            break
+        sha = element.get('sha')
+        author = element.get('commit').get('author').get('name')
+        print("{}: {}".format(sha, author))
+        i += 1
